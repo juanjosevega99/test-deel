3b478cdffdc714cb319f41af592a621373b492d7
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-04 22:58:28 -0500

    chore: add controller for contract

---
+++ src/controllers/contract.js
@@ -0,0 +1,38 @@
+const getContractById = async (req, res, next) => {
+  try {
+    const { Contract } = req.app.get('services');
+    const { id } = req.params;
+    const profileId = req.profile.id;
+
+    const contract = await Contract.getUserContract({
+      contractId: id,
+      profileId
+    });
+
+    if (!contract) {
+      return res.status(404).send({ error: 'Contract not found' });
+    }
+
+    res.json(contract);
+  } catch (error) {
+    return next(err);
+  }
+};
+
+const getActiveContracts = async (req, res, next) => {
+  try {
+    const { Contract } = req.app.get('services');
+    const profileId = req.profile.id;
+
+    const contracts = await Contract.getActiveContracts({ profileId });
+
+    res.json(contracts);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = {
+  getContractById,
+  getActiveContracts
+};
