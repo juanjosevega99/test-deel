a3d7362078342a5ba24f9a729346f3dd29f49e1a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-04 22:57:42 -0500

    refactor: create structure of app

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+
+const { sequelize } = require('./model');
+const { GetServices } = require('./services');
+const { Logger } = require('./logger');
+const router = require('./routes');
+const configs = require('./utils/constants');
+
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
+app.set('logger', Logger);
+
+const services = GetServices({
+  Models: sequelize.models,
+  Logger,
+  Configs: configs
+});
+
+app.set('services', services);
+
+app.use(router);
+
+app.use((err, req, res, next) => {
+  if (err) {
+    Logger.error('Something went wrong:', err);
+    return res.status(err.code).json({ code: err.message });
+  }
+  res.status(500).send('Internal Server Error');
+});
+
 module.exports = app;
