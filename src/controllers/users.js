1f5b1c209f78a78dcd82b15f887cc2b00763410a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-04 22:58:47 -0500

    chore: add controller for users

---
+++ src/controllers/users.js
@@ -0,0 +1,20 @@
+const depositBalance = async (req, res, next) => {
+  try {
+    const { User } = req.app.get('services');
+    const { userId } = req.params;
+    const { amount } = req.body;
+
+    if (typeof amount !== 'number') {
+      return res.status(400).send({ error: 'Amount should be a number' });
+    }
+
+    await User.depositBalance({ userId, amount });
+    res.sendStatus(200);
+  } catch (error) {
+    return next(err);
+  }
+};
+
+module.exports = {
+  depositBalance
+};
