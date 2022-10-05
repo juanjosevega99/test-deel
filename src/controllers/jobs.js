a2da7958b86d13f0e34a25d9bff0aa1e93259e49
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-04 22:58:41 -0500

    chore: add controller for jobs

---
+++ src/controllers/jobs.js
@@ -0,0 +1,29 @@
+const getUnpaidJobs = async (req, res, next) => {
+  try {
+    const { Job } = req.app.get('services');
+    const profileId = req.profile.id;
+
+    const jobs = await Job.getUnpaidJobs({ profileId });
+    res.status(200).json(jobs);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const payJob = async (req, res, next) => {
+  try {
+    const { Job } = req.app.get('services');
+    const { job_id: jobId } = req.params;
+    const profileId = req.profile.id;
+
+    await Job.payJob({ profileId, jobId });
+    res.sendStatus(200);
+  } catch (error) {
+    return next(err);
+  }
+};
+
+module.exports = {
+  getUnpaidJobs,
+  payJob
+};
