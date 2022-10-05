0f1c341e6ed93123a2ffadf022d5a5363efff676
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2022-10-04 23:05:19 -0500

    fix: getProfile working correctly

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,9 +1,17 @@
-
 const getProfile = async (req, res, next) => {
-    const {Profile} = req.app.get('models')
-    const profile = await Profile.findOne({where: {id: req.get('profile_id') || 0}})
-    if(!profile) return res.status(401).end()
-    req.profile = profile
-    next()
-}
-module.exports = {getProfile}
+  const { Profile } = req.app.get('models');
+  const profileId = req.get('profile_id');
+
+  if (!profileId) {
+    return res.status(401).send({ error: 'Missing profile_id header' });
+  }
+
+  const profile = await Profile.findOne({ where: { id: profileId } });
+
+  if (!profile) return res.status(401).send({ error: 'Invalid profile_id' });
+
+  req.profile = profile;
+  next();
+};
+
+module.exports = { getProfile };
